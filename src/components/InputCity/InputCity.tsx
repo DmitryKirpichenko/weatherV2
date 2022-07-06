a5fc3311ba78f21d71f82df36d92c902277405f1
DmitryKirpichenko <mr.dima.kirpichenko@gmail.com>
2022-07-06 23:09:58 +0300

    add: city hints

--- src/components/InputCity/InputCity.tsx
+++ src/components/InputCity/InputCity.tsx
@@ -1,19 +1,38 @@
+import axios from "axios";
 import React, { FC, useState } from "react";
+import { KEY } from "../../constants";
 import { useActions } from "../../hooks/useActions";
 import { StyledCityButton, StyledInput } from "./StyledInputCity";
+
+interface ICitys{
+    name:string
+    country: string
+}
 
 export const InputCity: FC<{ city: string }> = ({ city }) => {
     const { fetchWeather } = useActions()
 
     let [viewCity, setViewSity] = useState(city)
+    let [citysName, setcitysName] = useState<ICitys[]>([{name:city, country:''}])
+
+    let fetchCity = async (name:string) => {
+        await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${KEY}`).then(answer => setcitysName(answer.data))
+    }
 
     let handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setViewSity(e.target.value)
+        fetchCity(e.target.value)
     }
 
     return (
         <>
-            <StyledInput placeholder={city} onChange={(e) => handleChange(e)}></StyledInput>
+            <StyledInput list="citysList" placeholder={city} onChange={(e) => handleChange(e)}></StyledInput>
+            <datalist id="citysList">
+                {citysName.map((item) => (
+                    <option>{`${item.name},${item.country}`}</option>
+                ))}
+                
+            </datalist>
             <StyledCityButton onClick={() => fetchWeather(viewCity)}></StyledCityButton>
         </>
     )
